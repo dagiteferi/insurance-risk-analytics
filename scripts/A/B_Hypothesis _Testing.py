7d6abdc5e411bdd00aa0ff2eed03092ca0bf4e2d
Dagmawi Teferi <dagiteferi2011@gmail.com>
2024-12-27 22:31:19 -0500

    add data loading and preprocessing functions to hypothesis testing script

--- scripts/A/B_Hypothesis _Testing.py
+++ scripts/A/B_Hypothesis _Testing.py
@@ -1,3 +1,16 @@
 import pandas as pd
 from scipy.stats import chi2_contingency , ttest_ind , f_oneway
 import os
+
+#function to load data
+def data_load(filepath):
+    
+    return pd.read_csv(filepath)
+#function to see the null values
+def data_preprocess(data):
+    return data.isnull().sum()
+def categorical_outliers_detecting(data , column):
+    return data[column].unique()
+def handle_catagorical_outliers(data,column , valid_values , replecment_values=None):
+    invalid_entries = data[~data[column].isin(valid_values)]
+
