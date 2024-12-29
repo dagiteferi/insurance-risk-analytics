a16def25c4470873d5ae314804e7a30f4defa0c2
Dagmawi Teferi <dagiteferi2011@gmail.com>
2024-12-28 22:00:44 -0500

    add function to define parameter grid for GridSearchCV with multiple models

--- scripts/model.py
+++ scripts/model.py
@@ -12,3 +12,31 @@ def building_model():
     xgb_model = xgb.XGBRegressor(random_state=42)
 
     return lr_model,dt_model,rfr_model,xgb_model
+
+def define_parameter_grid_for_gridsearchcv(lr_model,dt_model,rfr_model,xgb_model):
+    param_grid_lr = {
+    'fit_intercept': [True]
+    }
+
+    param_grid_dt = {
+        'max_depth': [10],
+        'min_samples_split': [5]
+    }
+
+    param_grid_rf = {
+        'n_estimators': [100],
+        'max_depth': [10],
+        'min_samples_split': [5]
+    }
+
+    param_grid_xgb = {
+        'n_estimators': [100],
+        'max_depth': [5],
+        'learning_rate': [0.1]
+    }
+
+    lr_grid = GridSearchCV(lr_model,param_grid_lr,cv=5,scoring='r2')
+    dt_grid = GridSearchCV(dt_model, param_grid_dt, cv=5, scoring='r2')
+    rf_grid = GridSearchCV(rfr_model, param_grid_rf, cv=5, scoring='r2')
+    xgb_grid = GridSearchCV(xgb_model, param_grid_xgb, cv=5, scoring='r2')
+    return lr_grid,dt_grid,rf_grid,xgb_grid
