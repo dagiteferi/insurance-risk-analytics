eff506cf7b620ddc489195f9a1d479f526df2682
Dagmawi Teferi <dagiteferi2011@gmail.com>
2024-12-28 21:12:38 -0500

    add Statistical Modeling notebook and data processing script for Task 4

---
+++ scripts/model_data_process.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder , StandardScaler
+from sklearn.model_selection import  train_test_split
+from sklearn.preprocessing import OneHotEncoder
+from scipy import sparse
+from sklearn.decomposition import PCA
