022b5b98aa36717c1cf3472edb382a71c01974d9
Dagmawi Teferi <dagiteferi2011@gmail.com>
2024-12-29 19:07:23 -0500

    Add SHAP analysis for Decision Tree model in Feature_importance_analysis.py

--- scripts/Feature_importance_analysis.py
+++ scripts/Feature_importance_analysis.py
@@ -19,3 +19,13 @@ def shap_for_random_forest(rfr_model,x_test):
 
     # Visualize feature importance
     shap.summary_plot(shap_values_rf, x_test, plot_type="bar")
+
+def shap_for_decision_tree(dt_model,x_test):
+    #  an explainer for Decision Tree
+    explainer_dt = shap.TreeExplainer(dt_model)
+
+    #  SHAP values
+    shap_values_dt = explainer_dt.shap_values(x_test)
+
+    # Visualize feature importance
+    shap.summary_plot(shap_values_dt, x_test, plot_type="bar")
