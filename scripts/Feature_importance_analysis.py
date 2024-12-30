5b973524758cb61ee446a7f2931033770d0c5c3e
Dagmawi Teferi <dagiteferi2011@gmail.com>
2024-12-29 19:06:08 -0500

    Add SHAP analysis for Random Forest and improve comments in Feature_importance_analysis.py

--- scripts/Feature_importance_analysis.py
+++ scripts/Feature_importance_analysis.py
@@ -1,11 +1,21 @@
 import shap 
 
 def shap_for_XGBoost(xgb_model,x_test):
-    # Create an explainer for XGBoost
+    #explainer for XGBoost
     explainer_xgb = shap.Explainer(xgb_model)
 
-    # Compute SHAP values for test data
+    # Comute SHAP values for test data
     shap_values_xgb = explainer_xgb(x_test)
 
-    # Visualize feature importance (summary plot)
+    #  feature importance (summary plot)
     shap.summary_plot(shap_values_xgb, x_test, plot_type="bar")
+
+def shap_for_random_forest(rfr_model,x_test):
+   #  an explainer for Random Forest
+    explainer_rf = shap.TreeExplainer(rfr_model)
+
+    #  SHAP values
+    shap_values_rf = explainer_rf.shap_values(x_test)
+
+    # Visualize feature importance
+    shap.summary_plot(shap_values_rf, x_test, plot_type="bar")
